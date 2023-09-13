a01eca33f90a245992662017024f2f5ac480a3ba
Aarya Chauhan <aaryachauhan11@gmail.com>
2023-09-13 17:42:16 +0530

    layout designing

---
+++ src/MovieCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const movieCard = ({movie}) => {
+  return (
+    <div className="movie">
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <span></span>
+        <h3></h3>
+      </div>
+    </div>
+  );
+};
+
+export default movieCard;
